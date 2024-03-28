4079776eafe3a22448da27a6082b5cf2cca7bab0
Cao Xuân Kiên <kiencx@appota.com>
2024-03-28 16:08:54 +0700

    first commit

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:real_flutter_project/services/weather_service.dart';
+
+import '../models/weather_model.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  // api key
+  final _weatherService = WeatherService('1218b65841b2adc8355406d9991fa310');
+  Weather? _weather;
+
+  // fetch weather
+  _fetchWeather() async {
+    // get the current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    // get weather for the current city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == 'null') return 'assets/sunny.json';
+
+    switch (mainCondition?.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloudy.json';
+
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rainy.json';
+
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  // init state
+  @override
+  void initState() {
+    super.initState();
+
+    // fetch weather on startup
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(_weather?.cityName ?? 'loading ...',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+          Text('${_weather?.temperature.round()} °C',
+              style: TextStyle(fontSize: 25)),
+          Text(_weather?.mainCondition ?? ''),
+        ],
+      ),
+    ));
+  }
+}
